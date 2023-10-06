8088ebe7ba27c045d2dbea789076418d49baff62
Nico Stern <s92nster@uni-bonn.de>
2023-10-06 14:46:48 +0200

    basic django setup

---
+++ checkweb/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+def index(request):
+    return HttpResponse("yesh")
