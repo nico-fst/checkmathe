2a734fdec514762c6ac9ca1ce6bfa11749430f37
Nico Stern <s92nster@uni-bonn.de>
2023-11-28 01:07:43 +0100

    Models::Tutoring created + additional models

--- checkweb/admin.py
+++ checkweb/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import Role, User, Subject, Tutoring
 
 # Register your models here.
+admin.site.register(Role)
 admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Tutoring)
--- checkweb/models.py
+++ checkweb/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Role(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    preis_pro_45 = models.FloatField(null=True, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, blank=True, null=True)
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Tutoring(models.Model):
+    date = models.DateField()
+    duration = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="teaching_tutorings")
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="learning_tutorings")
+    content = models.TextField()
+
+    def __str__(self):
+        return f"({self.date}) {self.student} by {self.teacher} [{self.subject}]"
