275d2a818580be5d4df74c6d4064e56cb26fc257
Nico Stern <s92nster@uni-bonn.de>
2023-12-30 01:25:36 +0100

    API: init with example usage

--- checkweb/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Role, User, Subject, Tutoring
-
-# Register your models here.
-admin.site.register(Role)
-admin.site.register(User)
-admin.site.register(Subject)
-admin.site.register(Tutoring)
