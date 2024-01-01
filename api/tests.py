eb84eacf60d44d75cb2fcc0ee58e710924899c2b
Nico Stern <s92nster@uni-bonn.de>
2024-01-01 13:44:12 +0100

    (CORRECTED tests)

--- api/tests.py
+++ api/tests.py
@@ -2,14 +2,14 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from checkweb.models import Subject
+from checkweb.models import Subject, Tutoring, User
 from .serializers import SubjectSerializer
 
 
 class SubjectTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.subject_data = {"name": "Test Subject", "description": "Test Description"}
+        self.subject_data = {"title": "Test Subject"}
         self.url_get_subject = reverse("getSubject")
         self.url_add_subject = reverse("addSubject")
 
@@ -20,8 +20,9 @@ class SubjectTests(TestCase):
     def test_add_subject(self):
         response = self.client.post(self.url_add_subject, self.subject_data, format="json")
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(Subject.objects.all().count(), 1)
 
     def test_subject_serialization(self):
         subject = Subject.objects.create(title="Test Subject")
         serializer = SubjectSerializer(subject)
-        self.assertEqual(Subject.objects.all().count(), 1)
+        self.assertEqual(serializer.data, {'id': 1, 'title': 'Test Subject'})
