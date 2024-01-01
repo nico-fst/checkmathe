c137bc2fb671791b7972e49453b896b147f787e4
Nico Stern <s92nster@uni-bonn.de>
2024-01-01 20:04:43 +0100

    DB::Subject: ADDED title being unique

--- checkweb/models.py
+++ checkweb/models.py
@@ -36,7 +36,7 @@ class User(AbstractUser):
 
 
 class Subject(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.title
