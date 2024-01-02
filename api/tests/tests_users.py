e329ed10f72496fcde6955d8d4da9a90a3842515
Nico Stern <s92nster@uni-bonn.de>
2024-01-02 01:14:57 +0100

    Tests: RESTRUCTURED into folders with multiple files

---
+++ api/tests/tests_users.py
@@ -0,0 +1,115 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import Group
+from rest_framework import status
+from django.utils import timezone
+from rest_framework.test import APIClient
+from checkweb.models import Subject, Tutoring, User
+from ..serializers import SubjectSerializer
+from checkweb.views import calc_stundenkosten
+import json
+
+
+class UserViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.teacher_user = User.objects.create_user(
+            username="teacher_user",
+            password="password",
+            email="teacher@example.com",
+            first_name="teacher",
+            last_name="user",
+        )
+        self.teacher_user.groups.set([Group.objects.get(name="Teacher")])
+
+        self.student_user = User.objects.create_user(
+            username="student_user",
+            password="password",
+            email="student@example.com",
+            first_name="student",
+            last_name="user",
+        )
+
+    def test_get_user_teacher(self):
+        response = self.client.get(reverse("user_view", kwargs={"user_id": self.student_user.id}))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_nonexistent_user(self):
+        self.client.force_authenticate(user=self.teacher_user)
+        response = self.client.get(reverse("user_view", kwargs={"user_id": 999}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_post_user_teacher(self):
+        self.client.force_authenticate(user=self.teacher_user)
+        data = {
+            "username": "new_user",
+            "password": "password",
+            "email": "new_user@example.com",
+            "first_name": "John",
+            "last_name": "Doe",
+            "phone_number": "1234567890",
+        }
+        response = self.client.post(reverse("user_view"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_existing_user_teacher(self):
+        self.client.force_authenticate(user=self.teacher_user)
+        data = {
+            "username": self.student_user.username,
+            "password": "password",
+            "email": "new_user@example.com",
+            "first_name": "John",
+            "last_name": "Doe",
+            "phone_number": "1234567890",
+        }
+        response = self.client.post(reverse("user_view"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class DeleteUserViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.teacher_user = User.objects.create_user(
+            username="teacher_user",
+            password="password",
+            email="teacher@example.com",
+            first_name="teacher",
+            last_name="user",
+        )
+        self.teacher_user.groups.set([Group.objects.get(name="Teacher")])
+
+        self.student_user = User.objects.create_user(
+            username="student_user",
+            password="password",
+            email="student@example.com",
+            first_name="student",
+            last_name="user",
+        )
+
+    def test_delete_user_teacher(self):
+        self.client.force_authenticate(user=self.teacher_user)
+        response = self.client.delete(
+            reverse("delete_user", kwargs={"username": self.student_user.username})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_nonexistent_user(self):
+        self.client.force_authenticate(user=self.teacher_user)
+        response = self.client.delete(
+            reverse("delete_user", kwargs={"username": "nonexistent_user"})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_student_cannot_delete_other_student(self):
+        self.client.force_authenticate(user=self.student_user)
+        response = self.client.delete(
+            reverse("delete_user", kwargs={"username": self.teacher_user.username})
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_student_can_delete_self(self):
+        self.client.force_authenticate(user=self.student_user)
+        response = self.client.delete(
+            reverse("delete_user", kwargs={"username": self.student_user.username})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- checkweb/tests/tests_basic.py
+++ checkweb/tests/tests_basic.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import Group
 from django.utils import timezone
-from .models import User, Subject, Tutoring
+from ..models import User, Subject, Tutoring
 import datetime
 from django.urls import reverse
 from django.db.utils import IntegrityError
@@ -154,84 +154,3 @@ class AuthenticationTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Invalid username and/or password.")
-
-
-class TutoringViewsTestCase(TestCase):
-    def setUp(self):
-        self.stud = Group.objects.get(name="Student")
-        self.teach = Group.objects.get(name="Teacher")
-
-        self.teacher = User.objects.create_user(
-            "demo_teacher", "demo@mail.de", "password", first_name="demo", last_name="teacher"
-        )
-        self.teacher.groups.set([self.teach])
-        self.teacher.save()
-
-        self.student = User.objects.create(
-            username="demo_student", first_name="demo", last_name="student", email="demo2@mail.de", password="password"
-        )
-        self.student.groups.set([self.stud])
-        self.student.save()
-
-        self.subject = Subject.objects.create(title="Astrologie")
-        self.subject.save()
-
-        self.tut = Tutoring.objects.create(
-            date="2023-01-01",
-            duration=45,
-            subject=Subject.objects.create(title="TestSubject"),
-            student=self.student,
-            teacher=self.teacher,
-            content="Ananas dies das.",
-        )
-        self.tut.save()
-
-    # def test_view_illegal(self):
-    #     client = Client()
-    #     client.force_login(self.student)
-
-    #     response = client.get(reverse)
-
-    def test_create_legal(self):
-        client = Client()
-        client.force_login(self.student)
-
-        form_data = {
-            "date": "1984-01-01",
-            "duration": 45,
-            "subject": self.tut.id,  # ID instead of Object!
-            "teacher": self.teacher.id,
-            "student": self.student.id,
-            "content": "Demo tutoring session Lorem ipsum.",
-        }
-
-        # send POST to view
-        response = client.post(reverse("tutoring"), data=form_data)
-
-        self.assertEqual(response.status_code, 302)  # form submission successful?
-        self.assertEqual(Tutoring.objects.count(), 2)  # new Tutoring object created?
-
-        # Check if the Tutoring object has the correct attributes
-        new_tut = Tutoring.objects.get(subject=Subject.objects.get(title="Astrologie"))
-
-        self.assertEqual(new_tut.date, datetime.date(1984, 1, 1))
-        self.assertEqual(new_tut.duration, 45)
-        self.assertEqual(new_tut.teacher, self.teacher)
-        self.assertEqual(new_tut.student, self.student)
-        self.assertEqual(new_tut.content, "Demo tutoring session Lorem ipsum.")
-
-    def test_delete_illegal(self):
-        client = Client()
-        client.force_login(self.student)
-        response = client.delete(reverse("tutoring", args=[self.tut.id]))
-
-        self.assertEqual(response.status_code, 403)
-        self.assertTrue(Tutoring.objects.filter(id=self.tut.id).exists())  # validate that not deleted
-
-    def test_delete_legal(self):
-        client = Client()
-        client.force_login(self.teacher)
-        response = client.delete(reverse("tutoring", args=[self.tut.id]))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(Tutoring.objects.filter(id=self.tut.id).exists())  # validate deletion
