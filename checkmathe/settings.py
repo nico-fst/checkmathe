18b5973c505069476c389aaa32e8d68e76c40e09
Nico Stern <s92nster@uni-bonn.de>
2024-01-03 19:57:47 +0100

    (Settings: FIXED DB being empty)

--- checkmathe/settings.py
+++ checkmathe/settings.py
@@ -86,6 +86,8 @@ WSGI_APPLICATION = "checkmathe.wsgi.application"
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
