c9db3926be62536653dec058719e9de46f4d7097
Nico Stern <s92nster@uni-bonn.de>
2024-01-03 03:01:31 +0100

    Serializers: ADDED pdf support for Tutoring +TESTED

--- checkweb/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CheckwebConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "checkweb"
-
-    def ready(self):
-        import checkweb.signals
