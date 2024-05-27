94fa062dd2228603d2bc26305c8fdfc8a4898ea5
Nico Stern <s92nster@uni-bonn.de>
2024-05-28 01:10:17 +0200

    S3: Integrated Bucket - still Signature conflicts

--- checkmathe/settings.py
+++ checkmathe/settings.py
@@ -144,3 +144,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Configuring AWS S3
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
--- checkweb/models.py
+++ checkweb/models.py
@@ -107,7 +107,7 @@ class Tutoring(models.Model):
             "subject_title": self.subject.title,
             "teacher_username": self.teacher.username,
             "student_username": self.student.username,
-            "content": self.content,
+            "content": self.content, #TODO fix pdf hard coded url concatenation
             "pdf": "http://127.0.0.1:8000/" + self.pdf.url if self.pdf else None,
             "paid": self.paid,
             "paid_status": self.paid_status,
