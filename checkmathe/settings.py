66ede2c11a310f244cf50ea46de8a692b760b474
Nico Stern <s92nster@uni-bonn.de>
2024-05-31 01:47:47 +0200

    Local mode now disables postgres too - Closes #42

--- checkmathe/settings.py
+++ checkmathe/settings.py
@@ -107,6 +107,7 @@ if not LOCAL:
     }
     
     # Configuring AWS S3
+    
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
