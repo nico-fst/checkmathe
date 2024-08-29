c8e1b216ab75c5ed92e3e68a752316f27ab07d44
Nico Stern <nst.web@posteo.de>
2024-08-29 03:42:09 +0200

    (host of DB as container instead of localhost)

--- checkmathe/settings.py
+++ checkmathe/settings.py
@@ -101,7 +101,7 @@ if not LOCAL:
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST", "localhost"),
+            "HOST": os.getenv("DB_HOST", "db"),
             "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
